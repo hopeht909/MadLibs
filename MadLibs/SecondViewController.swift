89df4c0350bffaeba784528eebf910ff7b8c481a
hopeht909 <hopeht909@gmail.com>
2021-12-13 11:16:36 +0000

    MadLibs

---
+++ MadLibs/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  MadLibs
+//
+//  Created by admin on 12/12/2021.
+//
+
+import UIKit
+
+
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var adjectiveTF: UITextField!
+    @IBOutlet weak var verbTF: UITextField!
+    @IBOutlet weak var verb2TF: UITextField!
+    @IBOutlet weak var nounTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            
+    }
+    }
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var outputData: UILabel!
+    var out = "...."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func unwind(_ sender: UIStoryboardSegue) {
+        if let secondCV = sender.source as? SecondViewController {
+            if let adj = secondCV.adjectiveTF.text,  let ver1 = secondCV.verbTF.text,  let ver2 = secondCV.verb2TF.text, let noun = secondCV.nounTF.text {
+                
+                outputData.text = "We are having a perfectly \(adj) right now. Later we will \(ver1) and \(ver2) in the \(noun)"
+                
+            }}
+    }
+    
+    
+    
 }
 
